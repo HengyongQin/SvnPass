7fb7366161ecddc5a059f4134e035fb321b630bb
Samur <qinhengyong@yonghongtech.com>
2020-12-19 16:57:31 +0800

    init

---
+++ svnpass/src/main/java/com/samur/svnpass/SvnpassApplication.java
@@ -0,0 +1,13 @@
+package com.samur.svnpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvnpassApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(SvnpassApplication.class, args);
+   }
+
+}
